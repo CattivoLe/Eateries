b7c3ee567f2b368f4240c7c330c3454fcb2aa806
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-01-13 14:38:30 +0600

    TableViewCell Class

---
+++ Eateries/Controllers/EateriesTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  EateriesTableViewCell.swift
+//  Eateries
+//
+//  Created by Alexander Omelchuk on 13/01/2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class EateriesTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
