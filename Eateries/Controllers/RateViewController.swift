8ce661802f6bb448a3cd7d46e260870524a7fa09
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-01-14 04:35:06 +0600

    Размытие фона экрана оценки

---
+++ Eateries/Controllers/RateViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RateViewController.swift
+//  Eateries
+//
+//  Created by Alexander Omelchuk on 14/01/2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.view.insertSubview(blurEffectView, at: 1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
