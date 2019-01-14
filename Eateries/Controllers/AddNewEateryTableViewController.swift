7e9e5e8afb2efa7804144c14145ba2871985fe74
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-01-15 03:05:02 +0600

    Настройка контроллера NewRest

--- Eateries/Controllers/AddNewEateryTableViewController.swift
+++ Eateries/Controllers/AddNewEateryTableViewController.swift
@@ -11,13 +11,41 @@ import UIKit
 class AddNewEateryTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var adresTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    
+    @IBAction func toggelIsVisitedPressed(_ sender: UIButton) {
+        if sender == yesButton {
+            sender.backgroundColor = #colorLiteral(red: 0, green: 0.7301866559, blue: 0, alpha: 1)
+            noButton.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        } else {
+            sender.backgroundColor = #colorLiteral(red: 0.7344238162, green: 0.1379992366, blue: 0.148756355, alpha: 1)
+            yesButton.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        }
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        if nameTextField.text == "" || adresTextField.text == "" || typeTextField.text == "" {
+            let alert = UIAlertController(title: "Упс", message: "Не все поля заполнены", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: "unwindSegueFromNew", sender: self)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        yesButton.backgroundColor = #colorLiteral(red: 0, green: 0.7301866559, blue: 0, alpha: 1)
+        noButton.backgroundColor = #colorLiteral(red: 0.7344238162, green: 0.1379992366, blue: 0.148756355, alpha: 1)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        imageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        imageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage // Извлечение полученного изображения
         imageView.contentMode = .scaleAspectFill // Режим отображения
         imageView.clipsToBounds = true // Обрезать все что за рамками супервью
         dismiss(animated: true, completion: nil)
