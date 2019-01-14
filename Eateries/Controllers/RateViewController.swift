b34c427aad40c9e30135306ae848eb7b30d21793
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-01-14 16:30:34 +0600

    Анимация кнопок рейтинга

--- Eateries/Controllers/RateViewController.swift
+++ Eateries/Controllers/RateViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class RateViewController: UIViewController {
 
+    @IBOutlet weak var badButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var briliantBotton: UIButton!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // MARK: - Анимация кнопок
+        
+        let buttonArray = [badButton, goodButton, briliantBotton]
+        for (index, button) in buttonArray.enumerated() {
+            let delay = Double(index) * 0.2
+            UIView.animate(withDuration: 0.6, delay: delay, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                button?.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // MARK: - Анимация, стартовая позиция
+        
+        badButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        goodButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        briliantBotton.transform = CGAffineTransform(scaleX: 0, y: 0)
         
         // MARK: - Эффект размытия заднего фона
         
@@ -21,16 +43,4 @@ class RateViewController: UIViewController {
         blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         self.view.insertSubview(blurEffectView, at: 1)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
