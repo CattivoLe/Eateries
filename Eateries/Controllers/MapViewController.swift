d20eef730ddb280d0c8033874a67ab1522931cbb
Alexander Omelchuk <42239507+CattivoLe@users.noreply.github.com>
2019-01-14 17:26:54 +0600

    MapView Controller

---
+++ Eateries/Controllers/MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  Eateries
+//
+//  Created by Alexander Omelchuk on 14/01/2019.
+//  Copyright © 2019 Александр Омельчук. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
