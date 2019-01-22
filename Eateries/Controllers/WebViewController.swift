a0469a3c78537128e2427856649f46adcce36032
Alexander <cattivole@icloud.com>
2019-01-22 13:41:46 +0300

    ProgressBar in WebViewController

--- Eateries/Controllers/WebViewController.swift
+++ Eateries/Controllers/WebViewController.swift
@@ -15,6 +15,10 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     var progressView: UIProgressView!
     var url: URL!
     
+    deinit { // Отпустить наблюдатель если отпущен контроллер
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,6 +31,7 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         webView.load(request)
         webView.allowsBackForwardNavigationGestures = true // Разрешить жесты навигации
         
+        // Элементы
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
@@ -34,8 +39,17 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         let refreshButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         toolbarItems = [progressButton, flexibleSpacer, refreshButton]
         navigationController?.isToolbarHidden = false // Не скрывать Toolbar
-        
-        
-        
+        // Progress Bar
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    // Что делать когда происходит наблюдение
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title // После загрузки присвоить название странички в Title ViewController
     }
 }
